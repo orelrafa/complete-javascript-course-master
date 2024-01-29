7380e2bd4bd9b306b86b3c9b9d0abb731d5b74b0
doom <orelrafailov1998@gmail.com>
2024-01-29 18:18:20 +0200

    API changed

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -4,3 +4,16 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+/*
+IMPORTANT: API URL Change
+
+🚨 The base URL of the API used throughout this section has changed
+
+It's not a big deal, it's really just one small change. Instead of:
+
+https://restcountries.eu/rest/v2/
+
+It's now:
+
+https://countries-api-836d.onrender.com/countries/
+*/
